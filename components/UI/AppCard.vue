7848be4f1479612e67c844d63879c460def05c7d
Erne <ernesto92.mg@gmail.com>
2021-05-14 00:22:46 -0400

    feat add app card component

---
+++ components/UI/AppCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="app-card" :class="classes" :style="styles">
+    <slot name="default"> </slot>
+  </div>
+</template>
+
+<script>
+import { convertToUnit } from '~/utils/helpers'
+
+export default {
+  name: 'AppCard',
+  props: {
+    flat: {
+      type: Boolean,
+      default: false,
+    },
+    height: {
+      type: [Number, String],
+      default: 64,
+    },
+    hover: {
+      type: Boolean,
+      default: false,
+    },
+    // maxHeight: {
+    //   type: [Number, String],
+    //   default: '',
+    // },
+    maxWidth: {
+      type: [Number, String],
+      default: '100%',
+    },
+    // minHeight: {
+    //   type: [Number, String],
+    //   default: false,
+    // },
+    // minWidth: {
+    //   type: [Number, String],
+    //   default: false,
+    // },
+    width: {
+      type: [Number, String],
+      default: '',
+    },
+  },
+  computed: {
+    classes() {
+      const classes = {
+        'app-card-hover': this.hover,
+      }
+      return classes
+    },
+    styles() {
+      const classes = {
+        height: convertToUnit(this.height),
+        width: convertToUnit(this.width),
+        'max-width': convertToUnit(this.maxWidth),
+      }
+      return classes
+    },
+  },
+}
+</script>
+
+<style scoped>
+.app-card {
+  display: block;
+  max-width: 100%;
+  min-height: 64px;
+  outline: none;
+  position: relative;
+  white-space: normal;
+  padding: 16px 20px;
+  border-radius: 8px;
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+}
+.app-card.app-card-hover:hover {
+  box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
+    0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12) !important;
+}
+</style>
