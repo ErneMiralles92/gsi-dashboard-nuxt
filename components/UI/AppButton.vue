84f03537db14ba66ef01403ab46e6150c38015de
Erne <ernesto92.mg@gmail.com>
2021-05-16 19:16:21 -0400

    style: change app icon box-shadow

--- components/UI/AppButton.vue
+++ components/UI/AppButton.vue
@@ -152,8 +152,8 @@ export default {
 }
 
 .app-btn-icon:hover {
-  box-shadow: 0 5px 5px -3px rgb(0 0 0 / 20%), 0 8px 10px 1px rgb(0 0 0 / 14%),
-    0 3px 14px 2px rgb(0 0 0 / 12%);
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12) !important;
 }
 
 .app-btn.app-btn-disabled:not(.app-btn-flat):not(.app-btn-text) {
