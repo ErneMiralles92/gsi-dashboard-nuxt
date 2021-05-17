d64f34c45b094d342a8114a0a569a5bad8a0aeb1
Erne <ernesto92.mg@gmail.com>
2021-05-17 13:28:17 -0400

    feat: :fire: add goals section

---
+++ components/Dashboard/Goals.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="app-column">
+    <div class="app-row app-align-center pt-4">
+      <h3 class="text-title">Goals</h3>
+      <AppButton icon style="margin-left: 8px" :color="$colors.orange">
+        <AppIcon>mdi-plus</AppIcon>
+      </AppButton>
+    </div>
+    <div class="app-row app-align-center">
+      <AppSlider width="400px" :icon-color="$colors.blue">
+        <div class="app-row mx-n2 px-2">
+          <AppCard
+            v-for="(goal, index) in goals"
+            :key="`goal-${index}`"
+            class="mx-2 my-3"
+            hover
+            height="160px"
+            width="160px"
+          >
+            <div class="app-column fill-height app-align-start">
+              <span class="text-headline">${{ goal.amount }}</span>
+              <span class="text-caption grey-text">{{ goal.date }}</span>
+              <div class="app-spacer" />
+              <AppIcon
+                v-if="goal.icon"
+                class="mt-2"
+                large
+                :color="goal.icon.color"
+              >
+                {{ goal.icon.name }}
+              </AppIcon>
+              <span class="mt-3">{{ goal.name }}</span>
+            </div>
+          </AppCard>
+        </div>
+      </AppSlider>
+    </div>
+    <!-- <div class="app-row mt-3">
+      <div class="app-column col-left vertical-divider">
+        <AppCarousel
+          height="200px"
+          :icon-color="$colors.blue"
+          @changed="(index) => (currentCardIndex = index)"
+        >
+          <AppCard
+            v-for="(card, index) in cardList"
+            :key="index"
+            hover
+            height="100%"
+            width="100%"
+            :color="card.color"
+          >
+            <div class="app-row fill-height">
+              <div
+                class="app-column app-justify-space-between"
+                style="color: white"
+              >
+                <div class="app-row">Cloudcash Image</div>
+                <div class="app-row text-headline">
+                  {{ card.number }}
+                </div>
+                <div class="app-row mx-n3">
+                  <div class="app-column px-3 app-align-start">
+                    <span class="text-mini-caption">Card Holder</span>
+                    <span class="text-caption">{{ card.holder }}</span>
+                  </div>
+                  <div class="app-column px-3 app-align-start">
+                    <span class="text-mini-caption">Expire</span>
+                    <span class="text-caption">{{ card.expDate }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </AppCard>
+        </AppCarousel>
+      </div>
+      <div class="app-column app-align-end col-right">
+        <div
+          class="app-row app-align-end text-display-1"
+          style="text-align: right"
+          :style="{
+            color: $colors.blue,
+          }"
+        >
+          <span
+            class="text-subtitle"
+            style="text-align: right; margin-right: 4px"
+            >$</span
+          >
+          {{ cardList[currentCardIndex].currentBalance }}
+        </div>
+        <span
+          class="text-caption grey-text"
+          style="margin-bottom: 24px; text-align: right"
+        >
+          Current balance
+        </span>
+        <span
+          class="app-row font-weight-medium app-align-end text-title"
+          style="text-align: right"
+          :style="{
+            color: $colors.green,
+          }"
+        >
+          <span
+            class="text-subtitle"
+            style="text-align: right; margin-right: 4px"
+            >$</span
+          >
+          {{ cardList[currentCardIndex].income }}
+        </span>
+        <span
+          class="text-caption grey-text"
+          style="margin-bottom: 24px; text-align: right"
+          >Income</span
+        >
+        <span
+          class="app-row app-align-end text-title"
+          style="text-align: right"
+          :style="{
+            color: $colors.red,
+          }"
+        >
+          <span
+            class="text-subtitle"
+            style="text-align: right; margin-right: 4px"
+            >$</span
+          >
+          {{ cardList[currentCardIndex].outcome }}
+        </span>
+        <span class="text-caption grey-text" style="text-align: right"
+          >Outcome</span
+        >
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    goals: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,25 @@
 <template>
   <div class="app-row my-n6 mx-n3">
-    <div class="app-column py-6 px-3" style="flex: 1">
-      <Cards />
+    <div class="col-12 col-sm-6 py-6 px-3">
+      <Cards v-model="fakeData.cards" />
       <AppCard class="mt-6" height="10rem" width="100%"> </AppCard>
     </div>
-    <div class="app-column py-6 px-3" style="flex: 1">
-      <AppCard height="10rem" width="100%"> </AppCard>
+    <div class="col-12 col-sm-6 py-6 px-3">
+      <div class="app-row">
+        <Goals :goals="fakeData.goals" />
+      </div>
+      <AppCard class="mt-6" height="10rem" width="100%"> </AppCard>
       <AppCard class="mt-6" height="10rem" width="100%"> </AppCard>
     </div>
-    <!-- <AppCarousel style="width: 400px">
-      <AppCard
-        v-for="i in 7"
-        :key="i"
-        hover
-        height="5rem"
-        min-width="100px"
-        style="margin: 16px"
-        >{{ i }}
-      </AppCard>
-    </AppCarousel>
-    <AppSlider style="width: 400px">
-      <AppCard
-        v-for="i in 7"
-        :key="i"
-        hover
-        height="5rem"
-        min-width="100px"
-        style="margin: 16px"
-        >{{ i }}
-      </AppCard>
-    </AppSlider>
-    <AppProgressBar
-      color="#6D4C41"
-      rounded
-      value="68"
-      width="400"
-      height="16"
-    ></AppProgressBar>
-    <AppSwitch v-model="switchModel"></AppSwitch>
-    <AppButton text color="#1976D2" text-color="black">My Text</AppButton>
-    <AppButton icon rounded color="cyan" text-color="white">
-      <AppIcon color="green">mdi-account</AppIcon>
-    </AppButton> -->
   </div>
 </template>
 
 <script>
+import fakeData from '~/fakeData'
 export default {
   data() {
     return {
-      switchModel: true,
+      fakeData,
     }
   },
 }
